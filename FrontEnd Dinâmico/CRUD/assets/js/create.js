bedf1eb85d7ebb4836ae0bd348c94c1917737e96
Mariana Furriel <marianafurrielss@gmail.com>
2023-09-29 18:25:50 -0300

    Front form create

--- FrontEnd Dinâmico/CRUD/assets/js/create.js
+++ FrontEnd Dinâmico/CRUD/assets/js/create.js
@@ -1,5 +1,26 @@
-adicionar.addEventListener('click',()=>{
-  const produtoNovo = {
-    
+adicionar.addEventListener("click", (e) => {
+  e.preventDefault();
+  gerarProduto();
+});
+
+function gerarProduto() {
+  const produto = {
+    nome: nome.value,
+    qtd: quantidade.value,
+    img: linkImagem.value,
+    tipo: tipoProduto(),
+    data: data.value,
+    hora: hora.value,
+  };
+  const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+  produtos.push(produto);
+  localStorage.setItem("produtos", JSON.stringify(produtos));
+}
+
+function tipoProduto() {
+  var tipos = document.getElementsByName("tipo");
+
+  for (i = 0; i < tipos.length; i++) {
+    if (tipos[i].checked) return tipos[i].value;
   }
-})
+}
